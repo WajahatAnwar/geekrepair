5fd7122d5304c16de2e4598b3a4fe3354d5a3f19
wajahatAnwar <wajahat@ikarobar.com>
2018-08-29 01:06:23 +0500

    Adding Order Webhook

--- app/Objects/ShopifyWebhook.php
+++ app/Objects/ShopifyWebhook.php
@@ -10,29 +10,29 @@ class ShopifyWebhook {
     
     public static function registerAppUninstallWebhook()
     {
-        $postData = [
+        // $postData = [
             
-            "topic" => "app/uninstalled",
-            "address" => config('app.url') . '/shopify/webhook/app_uninstall',
-            "format" => "json"
-            
-        ];
-
-        // $postData2 = [
-            
-        //     "topic" => "orders/create",
-        //     "address" => config('app.url') . '/shopify/webhook/order_create',
+        //     "topic" => "app/uninstalled",
+        //     "address" => config('app.url') . '/shopify/webhook/app_uninstall',
         //     "format" => "json"
             
         // ];
+
+        $postData2 = [
+            
+            "topic" => "orders/create",
+            "address" => config('app.url') . '/shopify/webhook/order_create',
+            "format" => "json"
+            
+        ];
         
         // return Shopify::setShopUrl(session('myshopifyDomain'))
         //                ->setAccessToken(session('accessToken'))
         //                ->post('admin/webhooks.json' , [ "webhook" => $postData]);
         
-        // return Shopify::setShopUrl(session('myshopifyDomain'))
-        //                ->setAccessToken(session('accessToken'))
-        //                ->post('admin/webhooks.json' , [ "webhook" => $postData2]);
+        return Shopify::setShopUrl(session('myshopifyDomain'))
+                       ->setAccessToken(session('accessToken'))
+                       ->post('admin/webhooks.json' , [ "webhook" => $postData2]);
     }
     
 }
