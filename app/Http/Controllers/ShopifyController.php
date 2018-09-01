fe5ff7b07482f4dd7bc0b5c16395bb4976005ff1
wajahatAnwar <wajahat@ikarobar.com>
2018-09-01 11:24:00 +0500

    Adding test to check the functionality of the app

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -154,7 +154,7 @@ class ShopifyController extends Controller
 	public function test_function_for_order()
 	{
 		$product_id = "1452081643590";
-		$all_product_details = DB::Table('product_license_key')->where('product_id', $product_id );
+		$all_product_details = DB::Table('product_license_key');
 		dd($all_product_details);
 	}
 
