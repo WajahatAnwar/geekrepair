f482294d93a5ef8f49186ef2fe517f1cce8256c1
wajahatAnwar <wajahat@ikarobar.com>
2018-09-01 13:15:18 +0500

    Adding test to check the functionality of the app

--- app/Http/Controllers/ShopifyController.php
+++ app/Http/Controllers/ShopifyController.php
@@ -160,7 +160,7 @@ class ShopifyController extends Controller
 	public function test_function_for_order()
 	{
 		$product_id = "1452081643590";
-		$email =  "wajahat@fmail.com";
+		$email =  "wajahat@gmail.com";
 		$all_product_details = DB::Table('product_license_key')->select('product_id', 'product_name', 'license_key', 'resold')->where('product_id', $product_id)->get();
 		
 
