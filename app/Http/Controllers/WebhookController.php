48c3e55b66b012cb411be27bbdbedadf13c68799
wajahatAnwar <wajahat@ikarobar.com>
2018-09-01 16:29:11 +0500

    Configure

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -50,12 +50,15 @@ class WebhookController extends Controller
 			$contact_email = $payload['contact_email'];
 			$product_id = $payload['line_items']['0']["id"];
 
-			Log::info($order_id);
-			Log::info($contact_email);
-			Log::info($product_id);
+			// Log::info($order_id);
+			// Log::info($contact_email);
+			// Log::info($product_id);
 			
 			$all_product_details = DB::Table('product_license_key')->select('product_id', 'product_name', 'license_key', 'resold')->where('product_id', $product_id)->get();
-		
+			
+			// Log::info($order_id);
+			// Log::info($contact_email);
+			Log::info($all_product_details);
 			foreach($all_product_details as $product_detail)
 			{
 				$product_id2 = $product_detail->product_id;
