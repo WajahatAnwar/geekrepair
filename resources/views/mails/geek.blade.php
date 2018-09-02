a52538e5167add335a739a69db2984d357310241
wajahatAnwar <wajahat@ikarobar.com>
2018-09-02 23:48:25 +0500

    Mail

--- resources/views/mails/geek.blade.php
+++ resources/views/mails/geek.blade.php
@@ -1,7 +1,10 @@
 Hello <i>{{ $geek->receiver }}</i>,
 <p>Here is License Key for the product you just bought from geekrepair,nl</p>
-<?php $license_array = $geek->license_key; ?>
-@for($i = 0; $i < $license_array.lenght; $i++)
+<?php 
+$license_array = $geek->license_key; 
+$counting = count($license_array);
+?>
+@for($i = 0; $i < $counting; $i++)
 <h2><strong>{{ $license[$i] }}</strong></h2>
 @endfor
 <p><u>Values passed by With method:</u></p>
