939576c546c02712a0363261886f99b636eff216
wajahatAnwar <wajahat@ikarobar.com>
2018-09-02 23:46:12 +0500

    Mail

--- resources/views/mails/geek.blade.php
+++ resources/views/mails/geek.blade.php
@@ -1,9 +1,9 @@
 Hello <i>{{ $geek->receiver }}</i>,
 <p>Here is License Key for the product you just bought from geekrepair,nl</p>
 <?php $license_array = $geek->license_key; ?>
-@foreach( $license_array as $license)
-<h2><strong>{{ $license }}</strong></h2>
-@endforeach
+@for($i = 0; $i < $license_array.lenght; $i++)
+<h2><strong>{{ $license[$i] }}</strong></h2>
+@endfor
 <p><u>Values passed by With method:</u></p>
  
 Thank You,
