198fc1987e96f64f67b260ba431d3ed448d7cc90
wajahatAnwar <wajahat@ikarobar.com>
2018-09-02 14:07:37 +0500

    some important changes

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -44,57 +44,61 @@ class WebhookController extends Controller
 	    $hmacHeader = $request->server('HTTP_X_SHOPIFY_HMAC_SHA256');
 
 	    if (Shopify::verifyWebHook($data, $hmacHeader)) {
-			Log::info("Hook Called");
+			
 			$payload = json_decode($data , true);
 			$order_id = $payload['id'];
 			$email = $payload['contact_email'];
-			$line_item = $payload['line_items'];
-			Log::info($line_item);
-			// foreach($line_item as $product)
-			// {
-				// Log::info($product->product_id);
-				// $all_product_details = DB::Table('product_license_key')->select('product_id', 'product_name', 'license_key', 'resold')->where('product_id', $product)->get();
+			$product_id = $payload['line_items']['0']["product_id"];
+			$quantity = $payload['line_items']['0']["quantity"];
 			
-				// $email_sent = true;
-				// foreach($all_product_details as $product_detail)
-				// {
-				// 	Log::info("Loop Called");
-				// 	$license_key = $product_detail->license_key;
-				// 	$resold = $product_detail->resold;
-	
-				// 	$license_key_count = DB::Table('customer_product_keys')
-				// 		->select('product_id', 'license_key', 'customer_email')
-				// 				->where('license_key', $license_key)->count();
-				// 	if($license_key_count < $resold)
-				// 	{
-				// 		$validating_license_key = DB::Table('customer_product_keys')
-				// 		->select('product_id', 'license_key', 'customer_email')
-				// 			->where('product_id', $product_id)
-				// 				->where('license_key', $license_key)
-				// 					->where('customer_email', $email)->first();
-			
-				// 		if(empty($validating_license_key))
-				// 		{
-				// 			if($email_sent)
-				// 			{
-				// 				$id = DB::table('customer_product_keys')->insertGetId([
-				// 					'product_id' => $product_id,
-				// 					'license_key' => $license_key, 
-				// 					'customer_email' => $email,
-				// 					'created_at'=> date('Y-m-d H:i:s'), 
-				// 					'updated_at'=> date('Y-m-d H:i:s')
-				// 				]);
-								
-				// 				$this->send($email, $license_key);
-				// 				break 1;
-				// 			}else{
-				// 				Log::info("Email Already Sent");
-				// 				return false;
-				// 			}
-				// 		}
-				// 	}
-				// }
-			// }
+			$all_product_details = DB::Table('product_license_key')->select('product_id', 'product_name', 'license_key', 'resold')->where('product_id', $product_id)->get();
+			Log::info("Hook Called");
+			$email_sent = true;
+			for ($i=0; $i < $quantity ; $i++) { 
+				Log::info("Loop Called");
+				$license_key = $product_detail[$i]['license_key'];
+				$resold = $product_detail[$i]['resold'];
+				// $resold = $product_detail->resold;
+
+				$license_key_count = DB::Table('customer_product_keys')
+					->select('product_id', 'license_key', 'customer_email')
+							->where('license_key', $license_key)->count();
+				if($license_key_count < $resold)
+				{
+					$validating_license_key = DB::Table('customer_product_keys')
+					->select('product_id', 'license_key', 'customer_email')
+						->where('product_id', $product_id)
+							->where('license_key', $license_key)
+								->where('customer_email', $email)->first();
+		
+					if(empty($validating_license_key))
+					{
+						if($email_sent)
+						{
+							$id = DB::table('customer_product_keys')->insertGetId([
+								'product_id' => $product_id,
+								'license_key' => $license_key, 
+								'customer_email' => $email,
+								'created_at'=> date('Y-m-d H:i:s'), 
+								'updated_at'=> date('Y-m-d H:i:s')
+							]);
+							
+							$this->send($email, $license_key);
+							if($i >= $quantity)
+							{
+								break 1;
+							}
+						}else{
+							Log::info("Email Already Sent");
+							return false;
+						}
+					}
+				}
+			}
+			foreach($all_product_details as $product_detail)
+			{
+				
+			}
 			return new Response('Webhook Handled', 200);
 	    } else {
 	        Log::info('Webhook Request was not verified.');
