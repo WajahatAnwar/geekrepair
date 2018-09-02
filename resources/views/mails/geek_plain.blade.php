229e0046f83e8a73e8882ec5b1bc1f630d0e4577
wajahatAnwar <wajahat@ikarobar.com>
2018-09-02 23:07:14 +0500

    modifying email

--- resources/views/mails/geek_plain.blade.php
+++ resources/views/mails/geek_plain.blade.php
@@ -1,7 +1,10 @@
 Hello <i>{{ $geek->receiver }}</i>,
 <p>Here is License Key for the product you just bought from geekrepair,nl</p>
 
-<h2><strong>{{ $geek->license_key }}</strong></h2>
+<?php $license_array = $geek->license_key; ?>
+@foreach( $license_array as $license)
+<h2><strong>{{ $license }}</strong></h2>
+@endforeach
 <p><u>Values passed by With method:</u></p>
  
 Thank You,
