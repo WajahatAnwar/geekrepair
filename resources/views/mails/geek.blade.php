4366a93b3746ef9eaeddb311d77a06a02beb3b5d
wajahatAnwar <wajahat@ikarobar.com>
2018-09-03 00:09:21 +0500

    Mail

--- resources/views/mails/geek.blade.php
+++ resources/views/mails/geek.blade.php
@@ -1,4 +1,4 @@
-Hello <i>{{ $geek->receiver }}</i>,
+<!-- Hello <i>{{ $geek->receiver }}</i>,
 <p>Here is License Key for the product you just bought from geekrepair,nl</p>
 
 <h2><strong>{{ $geek->license_key }}</strong></h2>
@@ -6,4 +6,18 @@ Hello <i>{{ $geek->receiver }}</i>,
  
 Thank You,
 <br/>
-<i>{{ $geek->sender }}</i>
+<i>{{ $geek->sender }}</i> -->
+
+<p><strong>Beste</strong></p>
+
+<p>Bedankt voor je aankoop. Wij zullen deze met de grootste precisie behandelen om ervoor te zorgen dat jij tevreden bent met je product. 
+Hierbij de licenties die je aangeschaft hebt:</p>
+
+<h2><strong>{{ $geek->license_key }}</strong></h2>
+
+<p>Voor de installatiehandleiding en software van je producten kan je terecht op onze website <a href="www.geekrepair.nl">www.geekrepair.nl</a> of klik <a href="www.geekrepair.nl/pages/downloads">hier</a></p>
+
+<p>Graag zien we je spoedig terug bij Geekrepair. Indien je nog vragen hebt kan je altijd een mail sturen naar <a href="mailto:info@geekrepair.nl">mailto:info@geekrepair.nl</a></p>
+
+<p><strong>Met vriendelijke groet,</p></strong>
+<p><strong>Team Geekrepair</p></strong>
--- resources/views/mails/geek_plain.blade.php
+++ resources/views/mails/geek_plain.blade.php
@@ -1,4 +1,4 @@
-Hello <i>{{ $geek->receiver }}</i>,
+<!-- Hello <i>{{ $geek->receiver }}</i>,
 <p>Here is License Key for the product you just bought from geekrepair,nl</p>
 
 <h2><strong>{{ $geek->license_key }}</strong></h2>
@@ -6,4 +6,18 @@ Hello <i>{{ $geek->receiver }}</i>,
  
 Thank You,
 <br/>
-<i>{{ $geek->sender }}</i>
+<i>{{ $geek->sender }}</i> -->
+
+Beste,
+
+Bedankt voor je aankoop. Wij zullen deze met de grootste precisie behandelen om ervoor te zorgen dat jij tevreden bent met je product. 
+Hierbij de licenties die je aangeschaft hebt:
+
+<h2><strong>{{ $geek->license_key }}</strong></h2>
+
+Voor de installatiehandleiding en software van je producten kan je terecht op onze website <a href="www.geekrepair.nl">www.geekrepair.nl</a> of klik <a href="www.geekrepair.nl/pages/downloads">hier</a>
+
+Graag zien we je spoedig terug bij Geekrepair. Indien je nog vragen hebt kan je altijd een mail sturen naar <a href="mailto:info@geekrepair.nl">mailto:info@geekrepair.nl</a>
+
+Met vriendelijke groet,
+Team Geekrepair
