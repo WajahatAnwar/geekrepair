b0725a450b029f7fb76aad85684129947210cc5b
wajahatAnwar <wajahat@ikarobar.com>
2018-09-15 16:11:27 +0500

    Testing array by email

--- resources/views/mails/geek.blade.php
+++ resources/views/mails/geek.blade.php
@@ -1,12 +1,4 @@
-<!-- Hello <i>{{ $geek->receiver }}</i>,
-<p>Here is License Key for the product you just bought from geekrepair,nl</p>
 
-<h2><strong>{{ $geek->license_key }}</strong></h2>
-<p><u>Values passed by With method:</u></p>
- 
-Thank You,
-<br/>
-<i>{{ $geek->sender }}</i> -->
 {{ $geek }}
 <p><strong>Beste</strong></p>
 
