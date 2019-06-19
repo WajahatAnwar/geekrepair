c4c2d58988ea52fe19502bfaa79caf82d72b6cd9
wajahatAnwar <wajahat@ikarobar.com>
2019-06-19 15:00:25 +0500

    Web hook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -16,63 +16,59 @@ use Symfony\Component\HttpFoundation\Response;
 
 class WebhookController extends Controller
 {
-    public function app_uninstall(Request $request)
-    {
+	public function app_uninstall(Request $request)
+	{
 
-	    $data = $request->getContent();
+		$data = $request->getContent();
 
-	    $hmacHeader = $request->server('HTTP_X_SHOPIFY_HMAC_SHA256');
+		$hmacHeader = $request->server('HTTP_X_SHOPIFY_HMAC_SHA256');
 
-	    if (Shopify::verifyWebHook($data, $hmacHeader)) {
-	        
-	    	$payload = json_decode($data , true);
-	    	// $shop = Shop::where('shopify_id' , $payload['id'])->first();
-	    	// $shop->delete();
-	    	Log::info('Webhook Request verified and Handled.');
-	    	return new Response('Webhook Handled', 200);
+		if (Shopify::verifyWebHook($data, $hmacHeader)) {
 
-	    } else {
-	        Log::info('Webhook Request was not verified.');
-	    }
-
+			$payload = json_decode($data, true);
+			// $shop = Shop::where('shopify_id' , $payload['id'])->first();
+			// $shop->delete();
+			Log::info('Webhook Request verified and Handled.');
+			return new Response('Webhook Handled', 200);
+		} else {
+			Log::info('Webhook Request was not verified.');
+		}
 	}
-	
-	public function order_create(Request $request)
-    {
 
-	    $data = $request->getContent();
-	    $hmacHeader = $request->server('HTTP_X_SHOPIFY_HMAC_SHA256');
+	public function order_create(Request $request)
+	{
+
+		$data = $request->getContent();
+		$hmacHeader = $request->server('HTTP_X_SHOPIFY_HMAC_SHA256');
 		$email_sent = true;
 		$passing_array = array();
 
-	    if (Shopify::verifyWebHook($data, $hmacHeader)) {
+		if (Shopify::verifyWebHook($data, $hmacHeader)) {
 			Log::info("Hook Called");
-			$payload = json_decode($data , true);
+			$payload = json_decode($data, true);
 			$order_id = $payload['id'];
 			$email = $payload['contact_email'];
 			$product_id = $payload['line_items']['0']["product_id"];
 			$product_name = $payload['line_items']['0']["title"];
 			$quantity = $payload['line_items']['0']["quantity"];
-			
+			$license_key = "";
+			$resold = "";
 			$all_product_details = DB::Table('product_license_key')->select('product_id', 'product_name', 'license_key', 'resold')->where('product_id', $product_id)->get();
-			
+
 			$original_array = count($all_product_details);
-			for ($i=0; $i < $quantity ; $i++) 
-			{ 
-				
-				if(($i == $original_array) && ($quantity > $original_array))
-				{
+			for ($i = 0; $i < $quantity; $i++) {
+
+				if (($i == $original_array) && ($quantity > $original_array)) {
 					$loop_start = $quantity - $original_array;
-					for($p = 0; $p < $loop_start; $p++)
-					{
+					for ($p = 0; $p < $loop_start; $p++) {
 						$id = DB::table('customer_withno_keys')->insertGetId([
 							'product_name' 	=> $product_name,
-							'license_key' 	=> "0", 
-							'customer_email'=> $email,
+							'license_key' 	=> "0",
+							'customer_email' => $email,
 							'reason'        => "All keys are already assigned. No more keys available.",
-							'created_at'	=> date('Y-m-d H:i:s'), 
+							'created_at'	=> date('Y-m-d H:i:s'),
 							'updated_at'	=> date('Y-m-d H:i:s')
-						]);	
+						]);
 					}
 					break;
 					Log::info("Break not working");
@@ -80,89 +76,82 @@ class WebhookController extends Controller
 				Log::info("Loop Called");
 				$license_key;
 				$resold;
-				for($x = 0; $x <= $quantity; $x++)
-				{
-					if(!empty($all_product_details[$x]))
-					{
+				for ($x = 0; $x <= $quantity; $x++) {
+					if (!empty($all_product_details[$x])) {
 						$license_key = $all_product_details[$x]->license_key;
 						$resold = $all_product_details[$x]->resold;
 						$validating_license_key = DB::Table('customer_product_keys')
-						->select('product_id', 'license_key', 'customer_email')
+							->select('product_id', 'license_key', 'customer_email')
 							->where('product_id', $product_id)
-								->where('license_key', $license_key)
-									->where('customer_email', $email)->first();
-						if(empty($validating_license_key))
-						{
+							->where('license_key', $license_key)
+							->where('customer_email', $email)->first();
+						if (empty($validating_license_key)) {
 							Log::info("inner break works");
 							break 1;
 						}
 					}
 				}
-				
+
 
 				$license_key_count = DB::Table('customer_product_keys')
 					->select('product_id', 'license_key', 'customer_email')
-							->where('license_key', $license_key)->count();
-				if($license_key_count < $resold)
-				{
+					->where('license_key', $license_key)->count();
+				if ($license_key_count < $resold) {
 					$validating_license_key = DB::Table('customer_product_keys')
-					->select('product_id', 'license_key', 'customer_email')
+						->select('product_id', 'license_key', 'customer_email')
 						->where('product_id', $product_id)
-							->where('license_key', $license_key)
-								->where('customer_email', $email)->first();
-		
-					if(empty($validating_license_key))
-					{
-							$id = DB::table('customer_product_keys')->insertGetId([
-								'product_id' 	=> $product_id,
-								'license_key' 	=> $license_key, 
-								'customer_email'=> $email,
-								'created_at'	=> date('Y-m-d H:i:s'), 
-								'updated_at'	=> date('Y-m-d H:i:s')
-							]);
-							// email in one array
-							array_push($passing_array , $license_key);
-					}else if(!empty($validating_license_key))
-					{
+						->where('license_key', $license_key)
+						->where('customer_email', $email)->first();
+
+					if (empty($validating_license_key)) {
+						$id = DB::table('customer_product_keys')->insertGetId([
+							'product_id' 	=> $product_id,
+							'license_key' 	=> $license_key,
+							'customer_email' => $email,
+							'created_at'	=> date('Y-m-d H:i:s'),
+							'updated_at'	=> date('Y-m-d H:i:s')
+						]);
+						// email in one array
+						array_push($passing_array, $license_key);
+					} else if (!empty($validating_license_key)) {
 						$id = DB::table('customer_withno_keys')->insertGetId([
 							'product_name' 	=> $product_name,
-							'license_key' 	=> $license_key, 
-							'customer_email'=> $email,
+							'license_key' 	=> $license_key,
+							'customer_email' => $email,
 							'reason'        => "This Key is Already Assigned",
-							'created_at'	=> date('Y-m-d H:i:s'), 
+							'created_at'	=> date('Y-m-d H:i:s'),
 							'updated_at'	=> date('Y-m-d H:i:s')
-						]);	
+						]);
 					}
-				}else
-				{
+				} else {
 					$id = DB::table('customer_withno_keys')->insertGetId([
 						'product_name' 	=> $product_name,
-						'license_key' 	=> $license_key, 
-						'customer_email'=> $email,
+						'license_key' 	=> $license_key,
+						'customer_email' => $email,
 						'reason'        => "Can't resold this key more.",
-						'created_at'	=> date('Y-m-d H:i:s'), 
+						'created_at'	=> date('Y-m-d H:i:s'),
 						'updated_at'	=> date('Y-m-d H:i:s')
-					]);	
+					]);
 				}
 			}
-			if(!empty($passing_array)){
+			if (!empty($passing_array)) {
 				$this->send($email, $passing_array, $product_name);
 				return new Response('Webhook Handled', 200);
 			}
 			return new Response('Webhook Handled', 200);
-	    } else {
-	        Log::info('Webhook Request was not verified.');
-	    }
+		} else {
+			Log::info('Webhook Request was not verified.');
+		}
 	}
 
 	public function send($email, $license_key, $product_name)
-    {
-        $objDemo = new \stdClass();
-        $objDemo->email = $email;
-        $objDemo->license_key = $license_key;
-        $objDemo->product_name = $product_name;
-        $objDemo->sender = 'Geek Repair Store';
-        $objDemo->receiver = 'Valuable Customer';
+	{
+		$objDemo = new \stdClass();
+		$objDemo->email = $email;
+		$objDemo->license_key = $license_key;
+		$objDemo->product_name = $product_name;
+		$objDemo->sender = 'Geek Repair Store';
+		$objDemo->receiver = 'Valuable Customer';
 
 		$response = Mail::to($email)->send(new GeekEmail($objDemo));
 		return new Response('Webhook Handled', 200);
